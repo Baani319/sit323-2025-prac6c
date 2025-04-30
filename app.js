e0fcb7d8ca4b9630d6ce81ed992720926407b8a5
Baani <s223288898@deakin.edu.au>
2025-04-30 03:06:37 +0100

    Initial commit for SIT323 Kubernetes task

---
+++ app.js
@@ -0,0 +1,12 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from SIT323 Kubernetes App!');
+});
+
+app.listen(port, () => {
+  console.log(`App running at http://localhost:${port}`);
+});
